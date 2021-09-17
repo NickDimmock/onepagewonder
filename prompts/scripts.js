9a6a34f4f6af87c6260fac1a48427424fce1fbb1
Nick Dimmock <NickDimmock@users.noreply.github.com>
2021-09-17 16:11:58 +0100

    Add question features.

--- prompts/scripts.js
+++ prompts/scripts.js
@@ -1,5 +1,4 @@
-
-colors = [
+let colors = [
     "#FB5607",
     "#FF006E",
     "#8338EC",
@@ -7,11 +6,21 @@ colors = [
     "#8AC926"
 ]
 
+let questions = [
+    "What kind of story would it be?",
+    "Could it be a poem?",
+    "Who would it be about?",
+    "How would it start?",
+    "When would it take place?",
+    "Where would it take place?"
+]
+
 init = () => {
     document.getElementById("spin").onclick = () => {
         spin();
     }
     newPrompt();
+    newQuestions();
 }
 
 newPrompt = () => {
@@ -20,10 +29,19 @@ newPrompt = () => {
     do
         nounColor = _.sample(colors);
     while (adjColor == nounColor);
+    myAdj = _.sample(adjectives);
+    myNoun = _.sample(nouns);
     document.getElementById("adjective").style.color = adjColor;
     document.getElementById("noun").style.color = nounColor;
-    document.getElementById("adjective").innerText = _.sample(adjectives);
-    document.getElementById("noun").innerText = _.sample(nouns);
+    document.getElementById("adjective").innerText = myAdj;
+    document.getElementById("noun").innerText = myNoun;
+    
+}
+
+newQuestions = () => {
+    let myQuestions = _.sampleSize(questions,2);
+    let myTitle = document.getElementById("prompt").innerText;
+    document.getElementById("couldyou").innerHTML = `<p>Could you write a story called <strong>${myTitle}</strong>?</p><p>${myQuestions[0]}</p><p>${myQuestions[1]}</p><p>If you want to try again, press <strong>SPIN!</strong>`;
 }
 
 spin = () => {
@@ -33,6 +51,7 @@ spin = () => {
             newPrompt();
             if(i==9) {
                 document.getElementById("prompt").style.filter = "blur(0px)";
+                newQuestions();
             }
         }, 100*i);
     });
